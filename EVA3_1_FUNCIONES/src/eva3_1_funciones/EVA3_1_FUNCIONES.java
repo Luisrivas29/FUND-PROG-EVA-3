9a3e90408c8b128f59d900681be2636720bc810b
Luisrivas29 <143302710+Luisrivas29@users.noreply.github.com>
2023-11-21 19:52:04 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,21 @@
+package eva3_1_funciones;
+
+
+public class EVA3_1_FUNCIONES {
+
+   
+    public static void main(String[] args) {
+       
+        imprimirMensaje("HOLA MUNDO!!!");
+       
+    }
+    
+    public static void imprimirMensaje(String mensaje){
+        
+        System.out.println(mensaje);
+              
+     
+    }
+    
+    
+}
